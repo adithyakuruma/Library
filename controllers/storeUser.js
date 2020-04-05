218a284cdf546b5cae006a135b9e19d72cadc446
Murarishetty Bhanu Teja <murarishettybhanuteja@gmail.com>
2020-04-05 14:33:32 +0530

    added dark mode feature and solved profile pic issue

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = (req, res) => {
   const { image } = req.files
 
-  image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+  image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), (error) => {
     User.create({
       ...req.body,
       image: `/posts/${image.name}`,
